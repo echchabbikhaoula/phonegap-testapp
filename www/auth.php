e615e6eb23eda9f2b53ff4ec7f035e4756feff2a
khaoulaechchabbi <echabbi1995@gmail.com>
2021-01-06 11:45:13 +0100

    first commit

---
+++ www/auth.php
@@ -0,0 +1,31 @@
+<?php
+    $con = mysqli_connect("localhost","root","","testapp") or die("connection error");
+    header("Access-Control-Allow-Origin: *");
+    mysqli_connect("localhost","root","");
+    mysqli_select_db("testapp");
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    if(isset($_POST['register']))
+    {   
+        $register = mysqli_num_rows(mysqli_query($con, "SELECT * FROM `users` WHERE `email`='$email'"));
+        if($register == 0)
+        {
+            $insert = mysqli_query($con,"INSERT INTO `users` (`email`,`password`) VALUES ('$email','$password')");
+            if($insert)
+                echo "success";
+            else
+                echo "error";
+        }
+        else if($register != 0)
+            echo "exist";
+    }
+    else if(isset($_POST['login']))
+    {
+        $login = mysqli_num_rows(mysqli_query($con, "SELECT * FROM `users` WHERE `email`='$email' AND `password`='$password'"));
+        if($login != 0)
+            echo "success";
+        else
+            echo "error";
+    }
+    mysqli_close($con);
+?>
